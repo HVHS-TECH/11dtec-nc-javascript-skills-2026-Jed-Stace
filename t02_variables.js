3d15725326bf2217a595ef9df677d3a48d362776
Jed-Stace <24144js@hvhs.school.nz>
2026-04-30 21:34:12 +0000

    Apr 30, 2026, 9:34 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,13 @@
-/* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello!")
+
+let name = "Jed"
+let age = 15;
+let isstudent = true;
+
+// This is a single line comment
+
