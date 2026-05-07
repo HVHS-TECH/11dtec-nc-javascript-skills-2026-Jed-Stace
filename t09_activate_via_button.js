bca344da5d756bee16dbad38f07a9315f10f7ebe
Jed-Stace <24144js@hvhs.school.nz>
2026-05-07 21:43:44 +0000

    May 7, 2026, 9:43 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -16,15 +16,7 @@ let halfMoney = pocketMoney / 2;
 Main Code:
 ****************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 
-OUTPUT.innerHTML += ("<p>Hi " + username + "</p>")
-OUTPUT.innerHTML += ("<p>As of " + year + " you are " + age + " years old" + "</p>")
-OUTPUT.innerHTML += ("<p>You were born in " + (year - age) + "</p>")
-OUTPUT.innerHTML += ("<p>In 10 years you will be " + newAge + " years old" + "</p>")
-OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars" + "</p>")
-OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + "</p>")
-OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + "</p>")
 writeLine()
 /****************************
 Functions:
@@ -32,20 +24,23 @@ Functions:
 ****************************/
 
 function start(){
-    spaceForJavaScriptOutput.innerHTML = "<h2>Added by JavaScript</h2>";
-    spaceForJavaScriptOutput.innerHTML += ("<p>Hi " + username + "</p>")
-    answer = year - age
-    spaceForJavaScriptOutput.innerHTML += ("<p>You were born in " + answer + "</p>")
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += ("<p>Hi " + username + "</p>")
+OUTPUT.innerHTML += ("<p>As of " + year + " you are " + age + " years old" + "</p>")
+OUTPUT.innerHTML += ("<p>You were born in " + (year - age) + "</p>")
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + newAge + " years old" + "</p>")
+OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars" + "</p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + "</p>")
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + "</p>")
+displayProduct("Chocolate bar", "4")
+displayProduct("Chips", "3")
+displayProduct("Drink", "2.50")
+OUTPUT.innerHTML += "<p>Less code matters</p>"
 }
 
-function writeLine(){
-    OUTPUT.innerHTML += "<p>Less code matters</p>"
-}
+
 
 function displayProduct(_name, _price){
     OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
 }
 
-displayProduct("Chocolate bar", "4")
-displayProduct("Chips", "3")
-displayProduct("Drink", "2.50")
