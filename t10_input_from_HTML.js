7c4de807c7df605042cc935314f7f007e6584348
Jed-Stace <24144js@hvhs.school.nz>
2026-05-10 22:36:13 +0000

    May 10, 2026, 10:36 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -44,5 +44,9 @@ function displayProduct(_name, _price){
     OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
 }
 
-
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+}
 
